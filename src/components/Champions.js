da68704d764fb463cdb46b255701bb8d3b2a087d
zsomborr <zsomborkincses@gmail.com>
2021-05-17 17:38:37 +0200

    feature: free champions fetch(wip)
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>

--- src/components/Champions.js
+++ src/components/Champions.js
@@ -3,6 +3,7 @@ import Champion from "./Champion.js";
 
 const Champions = () => {
   const [champions, setChampions] = useState([]);
+  const [freeChampions, setFreeChampions] = useState([]);
 
   useEffect(() => {
     const getChampions = async () => {
@@ -10,7 +11,13 @@ const Champions = () => {
       setChampions(championsFromApi);
     };
 
+    // const getFreeChampions = async () => {
+    //   const championsFromApi = await fetchFreeChampions();
+    //   setFreeChampions(championsFromApi);
+    // };
+
     getChampions();
+    // getFreeChampions();
   }, []);
 
   const fetchChampions = async () => {
@@ -21,10 +28,38 @@ const Champions = () => {
     return data.data;
   };
 
+  // const fetchFreeChampions = async () => {
+  //   let myHeaders = new Headers();
+  //   myHeaders.append(
+  //     "X-Riot-Token",
+  //     "RGAPI-a0b13f9b-f31e-4516-86d1-b4937bd9a940"
+  //   );
+  //   myHeaders.append("Origin", "http://localhost:3000");
+
+  //   let requestOptions = {
+  //     method: "GET",
+  //     headers: myHeaders,
+  //     redirect: "follow",
+  //   };
+
+  //   const data = await fetch(
+  //     "https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations",
+  //     requestOptions
+  //   ).then((r) => r.json());
+  //   // const data = await res.json();
+  //   console.log(data);
+  //   return data.freeChampionIds;
+  // };
+
   return (
     <tbody>
       {Object.entries(champions).map(([key, value]) => (
-        <Champion key={key} champion={value} urlName={key} />
+        <Champion
+          key={key}
+          champion={value}
+          urlName={key}
+          freeChampions={freeChampions}
+        />
       ))}
     </tbody>
   );
