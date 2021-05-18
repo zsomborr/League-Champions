60ce692a22d98b50fe69bae007102c60a8664926
bodisB <bencebodis07@gmail.com>
2021-05-18 15:45:18 +0200

    Changed card colors
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/styles/CardStyle.js
+++ src/styles/CardStyle.js
@@ -4,13 +4,13 @@ export default styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 5px;
-  background-color: darkgray;
+  background-color: #2c5d72;
   text-align: center;
   width: 17%;
   padding: 10px;
   float: left;
   margin: 10px;
-  color: black;
+  color: gold;
   font-size: 1rem;
   &:hover {
     box-shadow: 0 10px 18px 5px #000;
