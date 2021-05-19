065a1a6732d9f4a1697fdfb0694866710f3afcff
bodisB <bencebodis07@gmail.com>
2021-05-19 10:51:41 +0200

    refactor: champions route
    Co-authored-by: zsomborr <zsomborr@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import "./styles/App.css";
 import Champions from "./components/Champions";
 import ChampionDetail from "./components/ChampionDetail";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Route path="/" exact component={Champions} />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Redirect to="/champions" />;
+          }}
+        />
+        <Route path="/champions" exact component={Champions} />
         <Route path="/championDetail/:championId" component={ChampionDetail} />
       </div>
     </Router>
