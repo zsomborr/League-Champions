4839fd8fe9165fc028a5428e1b4f92b7cdc7c797
bodisB <bencebodis07@gmail.com>
2021-05-19 11:37:46 +0200

    feature: added different style to free champions
    Co-authored-by: zsomborr <zsomborr@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,28 +1,51 @@
 import { Link } from "react-router-dom";
 import Card from "../styles/CardStyle.js";
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
 
 const Champion = ({ champion, freeChampions }) => {
+  const [theme, setTheme] = useState({
+    color: "gold",
+    backgroundColor: "#2c5d72",
+  });
+
+  useEffect(() => {
+    freeChampions.includes(champion.key)
+      ? setTheme((oldTheme) => ({
+          ...oldTheme,
+          color: "#2c5d72",
+          backgroundColor: "gold",
+        }))
+      : setTheme((oldTheme) => ({
+          ...oldTheme,
+          color: "gold",
+          backgroundColor: "#2c5d72",
+        }));
+  }, []);
+
   return (
-    <Link
-      to={{
-        pathname: `/championDetail/${champion.key}`,
-        state: { champion: champion },
-      }}
-    >
-      <Card>
-        <img
-          width="100%"
-          alt="test"
-          src={`https://ddragon.canisback.com/img/champion/tiles/${
-            champion.id === "Fiddlesticks" ? "FiddleSticks" : champion.id
-          }_0.jpg`}
-        ></img>
-        <br />
-        {champion.name}
-        <br />
-        {champion.title}
-      </Card>
-    </Link>
+    <ThemeProvider theme={theme}>
+      <Link
+        to={{
+          pathname: `/championDetail/${champion.key}`,
+          state: { champion: champion },
+        }}
+      >
+        <Card>
+          <img
+            width="100%"
+            alt="test"
+            src={`https://ddragon.canisback.com/img/champion/tiles/${
+              champion.id === "Fiddlesticks" ? "FiddleSticks" : champion.id
+            }_0.jpg`}
+          ></img>
+          <br />
+          {champion.name}
+          <br />
+          {champion.title}
+        </Card>
+      </Link>
+    </ThemeProvider>
   );
 };
 
