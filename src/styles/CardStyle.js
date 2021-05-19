69ee0a8c2be07422e0f1e9c2568569a0e5e006b4
bodisB <bencebodis07@gmail.com>
2021-05-19 11:38:37 +0200

    feature: added themeProvider
    Co-authored-by: zsomborr <zsomborr@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/styles/CardStyle.js
+++ src/styles/CardStyle.js
@@ -4,14 +4,14 @@ export default styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 5px;
-  background-color: #2c5d72;
+  background-color: ${(props) => props.theme.backgroundColor};
   text-align: center;
   width: 16.5%;
   padding: 10px;
   padding-bottom: 20px;
   float: left;
   margin: 10px;
-  color: gold;
+  color: ${(props) => props.theme.color};
   font-size: 1rem;
   &:hover {
     box-shadow: 0 10px 18px 5px #000;
