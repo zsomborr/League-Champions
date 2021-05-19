7223a6a6dbedadab89de5bc74ce8d2c44d22e7dd
bodisB <bencebodis07@gmail.com>
2021-05-19 10:54:01 +0200

    feature: search by tags
    Co-authored-by: zsomborr <zsomborr@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/components/Champions.js
+++ src/components/Champions.js
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
 import Champion from "./Champion.js";
 
-const Champions = () => {
+const Champions = (props) => {
   const [champions, setChampions] = useState([]);
-  const [freeChampions, setFreeChampions] = useState([]);
+  // const [freeChampions, setFreeChampions] = useState([]);
 
   useEffect(() => {
     const getChampions = async () => {
       const championsFromApi = await fetchChampions();
-      setChampions(championsFromApi);
+      const search = props.location.search;
+      let taggedChampions = [];
+      const params = new URLSearchParams(search);
+      const tag = params.get("tag");
+      if (tag !== null) {
+        Object.entries(championsFromApi).map((e) =>
+          e[1].tags.map((t) => (t === tag ? taggedChampions.push(e[1]) : null))
+        );
+      } else {
+        taggedChampions = Object.entries(championsFromApi).map((e) => e[1]);
+      }
+      setChampions(taggedChampions);
     };
 
-    const getFreeChampions = async () => {
-      const championsFromApi = await fetchFreeChampions();
-      setFreeChampions(championsFromApi);
-    };
+    // const getFreeChampions = async () => {
+    //   const championsFromApi = await fetchFreeChampions();
+    //   setFreeChampions(championsFromApi);
+    // };
 
     getChampions();
-    getFreeChampions();
-  }, []);
+    // getFreeChampions();
+  }, [props.location.search]);
 
   const fetchChampions = async () => {
     const res = await fetch(
@@ -28,33 +39,13 @@ const Champions = () => {
     return data.data;
   };
 
-  const fetchFreeChampions = async () => {
-    let myHeaders = new Headers();
-    myHeaders.append("X-Riot-Token", process.env.REACT_APP_RG_API_KEY);
-
-    let requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-
-    const data = await fetch(
-      "/api.riotgames.com/lol/platform/v3/champion-rotations",
-      requestOptions
-    ).then((r) => r.json());
-    // const data = await res.json();
-    console.log(data);
-    return data.freeChampionIds;
-  };
-
   return (
     <div>
-      {Object.entries(champions).map(([key, value]) => (
+      {champions.map((champion) => (
         <Champion
-          key={key}
-          champion={value}
-          urlName={key}
-          freeChampions={freeChampions}
+          key={champion.id}
+          champion={champion}
+          // freeChampions={freeChampions}
         />
       ))}
     </div>
