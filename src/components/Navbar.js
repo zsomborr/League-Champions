98e8a7062961b3468181b4e2f8d72e1bf41f146b
zsomborr <zsomborkincses@gmail.com>
2021-05-20 11:46:55 +0200

    feature: search bar inplemented in navbar
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
-import { Ul, Li, NavLink } from "../styles/NavBarStyle";
+import { Ul, Li, NavLink, InputBar } from "../styles/NavBarStyle";
 import Tags from "./Tags";
 import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useState } from "react";
 
 const Navbar = () => {
   const location = useLocation();
+
+  const [text, setText] = useState("");
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <Ul>
       <Li>
@@ -13,7 +20,21 @@ const Navbar = () => {
       <Li>
         <NavLink to="/favouriteChampions">Favourites</NavLink>
       </Li>
-      <Li>{location.pathname === "/champions" ? <Tags /> : null}</Li>
+      <Li>{location.pathname === "/champions" && <Tags />}</Li>
+      <Li>
+        <InputBar
+          type="search"
+          id="search"
+          name="search"
+          value={text}
+          onChange={handleOnChange}
+        />
+      </Li>
+      <Li>
+        <NavLink to={{ pathname: `/userDetail/?user=${text}` }}>
+          Search For User
+        </NavLink>
+      </Li>
     </Ul>
   );
 };
