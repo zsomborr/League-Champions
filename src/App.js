268bc5550d11d2385f49c4306a2d522df23c15a4
zsomborr <zsomborkincses@gmail.com>
2021-06-02 20:10:52 +0200

    feature: login feature togglers
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import { FavouriteProvider } from "./contexts/FavouriteContext";
 import FavouriteChampions from "./components/FavouriteChampions";
 import UserDetail from "./components/UserDetail";
 import LoginModal from "./components/LoginModal";
+import RegisterModal from "./components/RegisterModal";
 import { useState } from "react";
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const toggleLoginModal = () => {
     setShowLogin(!showLogin);
+  };
+
+  const toggleRegisterModal = () => {
+    setShowRegister(!showRegister);
   };
 
   const onLogin = () => {
@@ -23,6 +29,7 @@ function App() {
 
   const onLogout = () => {
     setIsLoggedIn(false);
+    sessionStorage.removeItem("user");
   };
 
   return (
@@ -30,6 +37,7 @@ function App() {
       <Router>
         <Navbar
           toggleLoginModal={toggleLoginModal}
+          toggleRegisterModal={toggleRegisterModal}
           onLogout={onLogout}
           isLoggedIn={isLoggedIn}
         />
@@ -46,6 +54,9 @@ function App() {
         <Route path="/userDetail" component={UserDetail} />
         {showLogin && (
           <LoginModal toggleLoginModal={toggleLoginModal} onLogin={onLogin} />
+        )}
+        {showRegister && (
+          <RegisterModal toggleRegisterModal={toggleRegisterModal} />
         )}
       </Router>
     </FavouriteProvider>
