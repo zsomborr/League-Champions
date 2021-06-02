6e2c1959d84eafab599dc4990e4d1e51c15f99cf
zsomborr <zsomborkincses@gmail.com>
2021-06-02 11:44:13 +0200

    feat: style for modal
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/styles/NavBarStyle.js
+++ src/styles/NavBarStyle.js
@@ -22,6 +22,17 @@ export const NavLink = styled(Link)`
   }
 `;
 
+export const NavElement = styled.div`
+  display: block;
+  color: #2c5d72;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  &:hover {
+    background-color: #ffdb0e;
+  }
+`;
+
 export const InputBar = styled.input`
   display: block;
   color: #2c5d72;
