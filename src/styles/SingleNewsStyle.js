c7dbb50907ca5116a8d7bc54c00fd2b1431397f8
bodisB <bencebodis07@gmail.com>
2021-06-03 10:53:40 +0200

    Feat: added news components
    Co-authored-by: zsomborr <zsomborr@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

---
+++ src/styles/SingleNewsStyle.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+  float: right;
+  color: yellow;
+  margin-right: 1%;
+`;
+
+export const SingleNewsCard = styled.div`
+  opacity: 0.9;
+  background-color: #2c5d72;
+  color: #d3b509;
+  padding: 1%;
+  margin: 1%;
+  width: 96%;
+  border-radius: 20px;
+  border: 1px solid #d3b509;
+  font-weight: 900;
+`;
+
+export const Title = styled.div`
+  font-size: 1.5rem;
+`;
+
+export const Data = styled.div`
+  font-size: 0.8rem;
+  font-style: italic;
+`;
